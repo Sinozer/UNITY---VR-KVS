e566196913b32ad20c0c65b4581735c2f27036b8
Théo <92313024+Sinozer@users.noreply.github.com>
2024-03-07 12:36:21 +0100

    chore(Assets), feat(Canva): Moved art assets to appropriate folders and created a canva for the product needed

---
+++ Assets/Game/Code/Scripts/UI/ProductUI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class ProductUI : MonoBehaviour
+{
+    [SerializeField] private UnityEngine.UI.Image _productImageHolder;
+    [SerializeField] private TMPro.TextMeshProUGUI _productNameHolder;
+
+    [SerializeField] private ItemSo _item;
+    
+    public ItemSo Item
+    {
+        get => _item;
+        set
+        {
+            _item = value;
+            // Get the image from the prefab model
+            _productImageHolder.sprite = _item.ItemImage;
+            _productNameHolder.text = _item.ItemName;
+        }
+    }
+    
+    #if UNITY_EDITOR
+    [Button]
+    private void Refresh()
+    {
+        if (_item == null)
+            return;
+        
+        _productImageHolder.sprite = _item.ItemImage;
+        _productNameHolder.text = _item.ItemName;
+    }
+    #endif
+}
