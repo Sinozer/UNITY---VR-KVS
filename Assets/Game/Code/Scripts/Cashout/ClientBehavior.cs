e2f8dc80bc5df14df24fd4116aa927e39ff078dc
tdelorme <thomasdelorme56@gmail.com>
2024-03-07 11:37:13 +0100

    feat: First implementation of Cashout state machine - Add waiting state

--- Assets/Game/Code/Scripts/Cashout/ClientBehavior.cs
+++ Assets/Game/Code/Scripts/Cashout/ClientBehavior.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
+using Sirenix.Serialization;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class ClientBehavior : MonoBehaviour
+public class ClientBehavior : SerializedMonoBehaviour
 {
-    [SerializeField, ReadOnly] private Dictionary<ItemSo, int> _shoppingList;
+    [OdinSerialize] private Dictionary<ItemSo, int> _shoppingList;
     [SerializeField, Range(1, 15)] private int _maxDifferentItems;
     [SerializeField, Range(1, 100)] private int _maxItemsPerCategory;
         
     private void Start()
     {
+        _shoppingList = new Dictionary<ItemSo, int>();
+        
         for (int i = 0; i < _maxDifferentItems; i++)
         {
             ItemSo randomItem = ItemRegistry.Instance.RandomItem;
