0b477ae6b84762615533151b9c24d109d4f795a9
tdelorme <thomasdelorme56@gmail.com>
2024-03-07 12:53:46 +0100

    refactor: Add getter for shopping list

--- Assets/Game/Code/Scripts/Cashout/ClientBehavior.cs
+++ Assets/Game/Code/Scripts/Cashout/ClientBehavior.cs
@@ -7,19 +7,28 @@ using Random = UnityEngine.Random;
 
 public class ClientBehavior : SerializedMonoBehaviour
 {
+    public Dictionary<ItemSo, int> ShoppingList => _shoppingList;
+    
+    public float DelayBetweenItems => _delayBetweenItems;
+    
+    [Header("Items to buy")] 
     [OdinSerialize] private Dictionary<ItemSo, int> _shoppingList;
+
     [SerializeField, Range(1, 15)] private int _maxDifferentItems;
     [SerializeField, Range(1, 100)] private int _maxItemsPerCategory;
-        
+
+    [Header("Client behavior")] 
+    [SerializeField, Range(0, 10)] private float _delayBetweenItems;
+
     private void Start()
     {
         _shoppingList = new Dictionary<ItemSo, int>();
-        
+
         for (int i = 0; i < _maxDifferentItems; i++)
         {
             ItemSo randomItem = ItemRegistry.Instance.RandomItem;
             if (_shoppingList.ContainsKey(randomItem)) return;
-                
+
             _shoppingList.Add(randomItem, Random.Range(1, _maxItemsPerCategory));
         }
     }
