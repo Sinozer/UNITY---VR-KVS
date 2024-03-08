7fea7436fe01fc9d7152cb90cea973dfa573dc4f
tdelorme <thomasdelorme56@gmail.com>
2024-03-08 13:02:16 +0100

    chore: Working on things

---
+++ Assets/Game/Code/Scripts/Cashout/Furnitures/FurnitureBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.Code.Scripts
+{
+    public class FurnitureBehavior : MonoBehaviour
+    {
+        private void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanBeGrabbed => _isGrabbed;
+        
+        private bool _isGrabbed;
+    }
+}
--- Assets/Game/Code/Scripts/Cashout/StateMachine/CashoutBrain.cs
+++ Assets/Game/Code/Scripts/Cashout/StateMachine/CashoutBrain.cs
@@ -1,8 +1,15 @@
-﻿using FiniteStateMachine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using FiniteStateMachine;
 using UnityEngine;
 
 public class CashoutBrain : StateMachine
 {
+    public float TotalScannedPrice { get; private set; }
+    
+    public event Action OnItemScanned;
+
     [SerializeField] private WaitingState _waitingState;
     [SerializeField] private SpawningItemsState _spawningItemsState;
     [SerializeField] private PaymentState _paymentState;
@@ -19,8 +26,46 @@ public class CashoutBrain : StateMachine
     [Header("Furniture")]
     [SerializeField] private BoxCollider _furnitureSpawnArea;
     
+    [Header("UI")]
+    [SerializeField] private TextAndPriceUI _lastProductDisplayUI;
+    [SerializeField] private ProductListUI _productListUI;
+    [SerializeField] private TextAndPriceUI _totalTextUI;
+    
+    [SerializeField] private List<ItemSo> _items;
+
+    public void RegisterItemToCashout(ItemSo product)
+    {
+        TotalScannedPrice += product.ItemPrice;
+        
+        _lastProductDisplayUI.SetTextAndPrice(product.ItemName, product.ItemPrice);
+        _productListUI.AddItemInfoToList(product);
+        
+        OnItemScanned?.Invoke();
+    }
+
+    public void ResetCashout()
+    {
+        TotalScannedPrice = 0;
+        
+        _productListUI.ResetRegisteredProducts();
+        _totalTextUI.ResetPrice();
+        _lastProductDisplayUI.ResetAll();
+    }
+
+    public void ShowTotal()
+    {
+        _lastProductDisplayUI.SetTextAndPrice("Total:", TotalScannedPrice);
+    }
+    
+    private void OnEnable()
+    {
+        _productListUI.OnProductListChanged += () => _totalTextUI.SetPrice(TotalScannedPrice);
+    }
+    
     private void Start()
     {
+        ResetCashout();
+        
         _clientHolder.Initialize(_spawnPoint);
         _clientHolder.StartSpawning();
         
