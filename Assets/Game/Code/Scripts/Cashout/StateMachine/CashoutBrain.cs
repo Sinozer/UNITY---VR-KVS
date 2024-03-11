0fd9bb0ad8bfbbc35efacea7532d42d9e443d33d
Hugo M <hmaestracci@gaming.tech>
2024-03-11 10:46:20 +0100

    Bugfix: Round total price

--- Assets/Game/Code/Scripts/Cashout/StateMachine/CashoutBrain.cs
+++ Assets/Game/Code/Scripts/Cashout/StateMachine/CashoutBrain.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using FiniteStateMachine;
+﻿using FiniteStateMachine;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -89,7 +86,7 @@ public class CashoutBrain : StateMachine
 
     private void UpdateTotalPriceText()
     {
-        _totalTextUI.SetPrice(TotalScannedPrice);
+        _totalTextUI.SetPrice(Mathf.Round(TotalScannedPrice * 100f) / 100f);
     }
 
     [Button]
