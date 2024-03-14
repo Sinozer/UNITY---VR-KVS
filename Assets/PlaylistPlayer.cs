6bb1dded162775e8b84b93b94d745110c14e783c
ArthurChassaing <94675172+ArthurChassaing@users.noreply.github.com>
2024-03-14 14:50:27 +0100

    Add microphone announcement

--- Assets/PlaylistPlayer.cs
+++ Assets/PlaylistPlayer.cs
@@ -1,27 +1,39 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class PlaylistPlayer : MonoBehaviour
 {
-    [SerializeField] private AudioSource _audioSource;
+    // [SerializeField] private AudioSource _audioSource;
+    [SerializeField, ReadOnly] private List<AudioSource> _audioSources;
     [SerializeField] private bool _isRandom;
     [SerializeField] private List<AudioClip> _playlist;
+    [SerializeField] private AudioClip _announcementSong; 
+    [SerializeField] private List<AudioClip> _broadcastMicrophone;
     
     private int _currentTrackIndex;
-    
+    private bool _isAnnouncementPlaying;
+
+    private void Awake()
+    {
+        _audioSources = GetComponentsInChildren<AudioSource>().ToList();
+    }
 
     private void Start()
     {
         _currentTrackIndex = _isRandom ? Random.Range(0, _playlist.Count) : 0;
         PlayMusic(_currentTrackIndex);
+        StartCoroutine(PlayBroadcastMicrophone());
     }
     
     private void Update()
     {
-        if (_audioSource.isPlaying) return;
-
+        if (_audioSources[0].isPlaying || _isAnnouncementPlaying) return;
+        
         if (_isRandom)
         {
             PlayRandomTrack();
@@ -97,28 +109,69 @@ public class PlaylistPlayer : MonoBehaviour
             Debug.LogError("Invalid clip!");
             return;
         }
-        
-        _audioSource.clip = clip;
-        _audioSource.Play();
+
+        foreach (AudioSource audioSource in _audioSources)
+        {
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+        // _audioSource.clip = clip;
+        // _audioSource.Play();
     }
 
     public void Stop()
     {
-        _audioSource.Stop();
+        foreach (AudioSource audioSource in _audioSources)
+        {
+            audioSource.Stop();
+        }
+        // _audioSource.Stop();
     }
     
     public void Pause()
     {
-        _audioSource.Pause();
+        foreach (AudioSource audioSource in _audioSources)
+        {
+            audioSource.Pause();
+        }
+        // _audioSource.Pause();
     }
     
     public void Resume()
     {
-        _audioSource.UnPause();
+        foreach (AudioSource audioSource in _audioSources)
+        {
+            audioSource.UnPause();
+        }
+        // _audioSource.UnPause();
     }
     
     public void SetVolume(float volume)
     {
-        _audioSource.volume = volume;
+        foreach (AudioSource audioSource in _audioSources)
+        {
+            audioSource.volume = volume;
+        }
+        // _audioSource.volume = volume;
+    }
+    
+    IEnumerator PlayBroadcastMicrophone()
+    {
+        while (true)
+        {
+            //yield return new WaitForSeconds(Random.Range(60, 125));
+            yield return new WaitForSeconds(5);
+            Pause();
+            _isAnnouncementPlaying = true;
+            PlayMusic(_announcementSong);
+            
+            yield return new WaitForSeconds(_announcementSong.length + 1);
+            
+            AudioClip randomBroadcast = _broadcastMicrophone[Random.Range(0, _broadcastMicrophone.Count)];
+            PlayMusic(randomBroadcast);
+            
+            yield return new WaitForSeconds(randomBroadcast.length + 1);
+            _isAnnouncementPlaying = false;
+        }
     }
 }
